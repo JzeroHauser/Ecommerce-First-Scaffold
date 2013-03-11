db7ea2f604cafac432ba4f43f2b967385fe8fa77
unknown <jhauser40@ITSM1034725.ACADEMIC.RRC.CA>
2013-03-11 15:10:25 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommScaffold::Application.config.secret_token = '74bba410a6e957d602084db384a1b1667f90e676bc15fb2a236f3b62178a7ee08c0f1981a4bb6c31c9ce32858101288ace2d9d2f60fbe3149c1854711d534530'
